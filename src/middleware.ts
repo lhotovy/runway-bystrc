fb008f8d87ebeaa9db4ae6d67a3b59ec0edd1d9b
lhotovy <lhotovy@centrum.cz>
2025-03-11 08:59:18 +0100

    fix: :label: Fixed type error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { activities } from './data/activities/data';
+import { activities } from './data/activities';
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
