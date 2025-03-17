18585b3163e3f9ed91656f8d27afe3795cfe087e
lhotovy <lhotovy@centrum.cz>
2025-03-17 15:16:02 +0100

    Added Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 import { Open_Sans } from 'next/font/google'
 import { useState } from 'react';
 import { Footer } from '@/components/footer/footer';
+import { Analytics } from "@vercel/analytics/react"
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         <Header onMobileMenuToggle={() => setMobileMenuOpen(!mobileMenuOpen)} />
         <main className="max-w-[1280px] mx-auto px-6 xl:px-0">
           {children}
+          <Analytics />
         </main> 
         <Footer />
       </body>
