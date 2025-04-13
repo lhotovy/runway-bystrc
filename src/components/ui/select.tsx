394042b7bf3936af3fcfa1f8e18f6bce4b416d1f
lhotovy <lhotovy@centrum.cz>
2025-04-13 22:12:51 +0200

    Challenge camp registration from

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,159 +1,109 @@
-"use client"
+import React from "react";
+import Select from "react-select";
+import { cn } from "@/lib/utils";
 
-import * as React from "react"
-import * as SelectPrimitive from "@radix-ui/react-select"
-import { Check, ChevronDown, ChevronUp } from "lucide-react"
+export const DropdownSelect = React.forwardRef((props: any, ref: any) => {
+  const currentOption = props?.options?.find((option: any) => option.key === props?.value);
 
-import { cn } from "@/lib/utils"
+  const customStyles = {
+    ...props?.styles,
+    menu: (provided: any) => ({
+      ...provided,
+      backgroundColor: "hsl(var(--color-base-100))",
+      minWidth: props?.minWidth,
+      textTransform: "capitalize",
+      fontSize: "0.875rem",
+    }),
+    option: (provided: any, state: any) => ({
+      ...provided,
+      color: state.isFocused
+      ? "hsl(var(--color-accent-content))"
+      : "hsl(var(--color-base-content))",
+      paddingLeft: 16,
+      paddingRight: 16,
+      fontSize: "0.875rem",
+      cursor: "pointer",
+      textTransform: "capitalize",
+      backgroundColor: state.isFocused ? "hsl(var(--color-accent))" : "hsl(var(--color-base-100))",
+      "&:hover": {
+        backgroundColor: "hsl(var(--color-accent))",
+        color: "hsl(var(--color-accent-content))",
+      },
+    }),
+    control: (provided: any, state: any) => ({
+      ...provided,
+      backgroundColor: "hsl(var(--color-base-100))",
+      minHeight: "32px",
+      //minWidth: "150px",
+      fontSize: "0.875rem",
+      height: "32px",
+      cursor: "pointer",
+      boxShadow: null,
+      textTransform: "capitalize",
+      border: "1px solid hsl(var(--color-gray-9))",
+      borderColor: "black",
+      "&:hover": {
+        border: state.isFocused ? `1px solid hsl(var(--color-accent))` : "1px solid rgb(29, 30, 58)",
+      },
+    }),
+    valueContainer: (provided: any) => ({
+      ...provided,
+      height: "32px",
+      padding: "0 6px",
+      maxWidth: "auto",
+      minWidth: "auto",
+    }),
+    input: (provided: any) => ({
+      ...provided,
+      margin: "0px",
+      color: "hsl(var(--color-accent))",
+      minWidth: "10rem",
+      maxWidth: "10rem",
+      textTransform: "capitalize",
+      fontSize: "12px",
+    }),
+    indicatorSeparator: () => ({
+      display: "none",
+    }),
+    indicatorsContainer: (provided: any) => ({
+      ...provided,
+      height: "30px",
+    }),
+    singleValue: (provided: any) => ({
+      ...provided,
+      fontSize: "0.875rem",
+      color: "hsl(var(--color-accent))",
+      textTransform: "capitalize",
+    }),
+    placeholder: (provided: any) => ({
+      ...provided,
+      color: "hsl(var(--color-accent))",
+      fontSize: "14px",
+    }),
+  };
 
-const Select = SelectPrimitive.Root
-
-const SelectGroup = SelectPrimitive.Group
-
-const SelectValue = SelectPrimitive.Value
-
-const SelectTrigger = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <SelectPrimitive.Trigger
-    ref={ref}
-    className={cn(
-      "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-xs ring-offset-background placeholder:text-muted-foreground focus:outline-hidden focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
-      className
-    )}
-    {...props}
-  >
-    {children}
-    <SelectPrimitive.Icon asChild>
-      <ChevronDown className="h-4 w-4 opacity-50" />
-    </SelectPrimitive.Icon>
-  </SelectPrimitive.Trigger>
-))
-SelectTrigger.displayName = SelectPrimitive.Trigger.displayName
-
-const SelectScrollUpButton = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.ScrollUpButton>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.ScrollUpButton
-    ref={ref}
-    className={cn(
-      "flex cursor-default items-center justify-center py-1",
-      className
-    )}
-    {...props}
-  >
-    <ChevronUp className="h-4 w-4" />
-  </SelectPrimitive.ScrollUpButton>
-))
-SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName
-
-const SelectScrollDownButton = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.ScrollDownButton>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollDownButton>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.ScrollDownButton
-    ref={ref}
-    className={cn(
-      "flex cursor-default items-center justify-center py-1",
-      className
-    )}
-    {...props}
-  >
-    <ChevronDown className="h-4 w-4" />
-  </SelectPrimitive.ScrollDownButton>
-))
-SelectScrollDownButton.displayName =
-  SelectPrimitive.ScrollDownButton.displayName
-
-const SelectContent = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
-  <SelectPrimitive.Portal>
-    <SelectPrimitive.Content
-      ref={ref}
-      className={cn(
-        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-        position === "popper" &&
-          "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
-        className
-      )}
-      position={position}
-      {...props}
-    >
-      <SelectScrollUpButton />
-      <SelectPrimitive.Viewport
-        className={cn(
-          "p-1",
-          position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
-        )}
+  return (
+    <div className={cn("flex-col", props.isHidden ? "hidden" : "flex")} 
       >
-        {children}
-      </SelectPrimitive.Viewport>
-      <SelectScrollDownButton />
-    </SelectPrimitive.Content>
-  </SelectPrimitive.Portal>
-))
-SelectContent.displayName = SelectPrimitive.Content.displayName
+      {props?.text && <label htmlFor={props.id} className="text-base-300 font-normal mb-1">{props.text}</label>}
+      
+      <Select
+        {...props}    
+        styles={customStyles}
+        value={{label: currentOption?.label}}
+        onChange={props?.onChange}
+        innerRef={ref}
+      />
+      {props?.errorMessage && (
+        <label          
+          className="text-error text-xs opacity-100 mt-1">
+          {props.errorMessage}
+        </label>
+      )}
+    </div>
+  );
+});
 
-const SelectLabel = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Label>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Label>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.Label
-    ref={ref}
-    className={cn("px-2 py-1.5 text-sm font-semibold", className)}
-    {...props}
-  />
-))
-SelectLabel.displayName = SelectPrimitive.Label.displayName
+DropdownSelect.displayName = "DropdownSelect";
 
-const SelectItem = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
->(({ className, children, ...props }, ref) => (
-  <SelectPrimitive.Item
-    ref={ref}
-    className={cn(
-      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-hidden focus:bg-accent focus:text-accent-foreground data-disabled:pointer-events-none data-disabled:opacity-50",
-      className
-    )}
-    {...props}
-  >
-    <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
-      <SelectPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
-      </SelectPrimitive.ItemIndicator>
-    </span>
-    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
-  </SelectPrimitive.Item>
-))
-SelectItem.displayName = SelectPrimitive.Item.displayName
-
-const SelectSeparator = React.forwardRef<
-  React.ElementRef<typeof SelectPrimitive.Separator>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Separator>
->(({ className, ...props }, ref) => (
-  <SelectPrimitive.Separator
-    ref={ref}
-    className={cn("-mx-1 my-1 h-px bg-muted", className)}
-    {...props}
-  />
-))
-SelectSeparator.displayName = SelectPrimitive.Separator.displayName
-
-export {
-  Select,
-  SelectGroup,
-  SelectValue,
-  SelectTrigger,
-  SelectContent,
-  SelectLabel,
-  SelectItem,
-  SelectSeparator,
-  SelectScrollUpButton,
-  SelectScrollDownButton,
-}
+export default DropdownSelect;
--- src/lib/functions.tsx
+++ src/lib/functions.tsx
@@ -1,3 +1,4 @@
+import DropdownSelect from "@/components/ui/select";
 import { JSX } from "react";
 
 export const generateFormElements = (definitions: { type: string, name: string, text: string, width: number, required: boolean }[], formData: any, setFormData: any) => {
@@ -196,6 +197,100 @@ export const generateFormElements = (definitions: { type: string, name: string, 
           className="shadow appearance-none border border-gray-3 rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
         />
       </div>
+    ),
+    preferedActivity: (name: string, text: string, required: boolean, width: number) => (
+      <div key={name} className={`flex flex-col mb-4`} style={{ width: `${width}px` }}>
+        <label className="block text-gray-700 text-base font-bold mb-2" htmlFor={name}>
+          {text}
+        </label>
+        <select       
+          name={name}
+          required={required}
+          value={formData[name] || ""}
+          onChange={(value: any) => setFormData({ ...formData, [name]: value })}
+          className="shadow appearance-none border border-gray-3 rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full bg-white bg-no-repeat bg-right"
+          style={{ backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='gray'%3E%3Cpath fill-rule='evenodd' d='M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z' clip-rule='evenodd'/%3E%3C/svg%3E")`, backgroundSize: "24px", backgroundPosition: "calc(100% - 8px) center" }}
+        >
+          <option value="" disabled>Vyber možnost</option>
+          <option value="sporty">Sporty</option>
+          <option value="crafts">Crafts (vyrábění)</option>
+        </select>
+      </div>
+    ),
+    shirtSize: (name: string, text: string, required: boolean, width: number) => (
+      <div key={name} className={`flex flex-col mb-4`} style={{ width: `${width}px` }}>
+        <label className="block text-gray-700 text-base font-bold mb-2" htmlFor={name}>
+          {text}
+        </label>
+        <input
+          type="text"
+          id={name}
+          name={name}
+          required={required}
+          value={formData[name] || ""}
+          onChange={(e) => setFormData({ ...formData, [name]: e.target.value })}
+          className="shadow appearance-none border border-gray-3 rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
+        />
+      </div>
+    ),
+    referal: (name: string, text: string, required: boolean, width: number) => (
+      <div key={name} className={`flex flex-col mb-4`} style={{ width: `${width}px` }}>
+        <label className="block text-gray-700 text-base font-bold mb-2" htmlFor={name}>
+          {text}
+        </label>
+        <select       
+          name={name}
+          required={required}
+          value={formData[name] || ""}
+          onChange={(value: any) => setFormData({ ...formData, [name]: value })}
+          className="shadow appearance-none border border-gray-3 rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full bg-white bg-no-repeat bg-right"
+          style={{ backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='gray'%3E%3Cpath fill-rule='evenodd' d='M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z' clip-rule='evenodd'/%3E%3C/svg%3E")`, backgroundSize: "24px", backgroundPosition: "calc(100% - 8px) center" }}
+        >
+          <option value="" disabled>Vyber možnost</option>
+          <option value="sporty">Z doporučení</option>
+          <option value="crafts">Z tištěných médií</option>
+          <option value="crafts">Facebook</option>
+          <option value="crafts">Instagram</option>
+          <option value="crafts">Jinde</option>
+        </select>
+      </div>
+    ),
+    recommendation: (name: string, text: string, required: boolean, width: number) => (
+      <div key={name} className={`flex flex-col mb-4`} style={{ width: `${width}px` }}>
+        <label className="block text-gray-700 text-base font-bold mb-2" htmlFor={name}>
+          {text}
+        </label>
+        <input
+          type="text"
+          id={name}
+          name={name}
+          required={required}
+          value={formData[name] || ""}
+          onChange={(e) => setFormData({ ...formData, [name]: e.target.value })}
+          className="shadow appearance-none border border-gray-3 rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full"
+        />
+      </div>
+    ),
+    englishLevel: (name: string, text: string, required: boolean, width: number) => (
+      <div key={name} className={`flex flex-col mb-4`} style={{ width: `${width}px` }}>
+      <label className="block text-gray-700 text-base font-bold mb-2" htmlFor={name}>
+        {text}
+      </label>
+      <select       
+        name={name}
+        required={required}
+        value={formData[name] || ""}
+        onChange={(value: any) => setFormData({ ...formData, [name]: value })}
+        className="shadow appearance-none border border-gray-3 rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full bg-white bg-no-repeat bg-right"
+        style={{ backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='gray'%3E%3Cpath fill-rule='evenodd' d='M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z' clip-rule='evenodd'/%3E%3C/svg%3E")`, backgroundSize: "24px", backgroundPosition: "calc(100% - 8px) center" }}
+      >
+        <option value="" disabled>Vyber možnost</option>
+        <option value="sporty">Neovládá</option>
+        <option value="crafts">Základní</option>
+        <option value="crafts">Pokročilá</option>
+        <option value="crafts">Výborná</option>
+      </select>
+    </div>
     ),
     // Add more form element types to the library as needed
   };
