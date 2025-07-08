b32b18bfbd8c494daac9ef540f2d61f7eec2cd89
lhotovy <lhotovy@centrum.cz>
2025-07-08 08:13:22 +0200

    Intrduced hero

--- src/components/activities/templates/template1.tsx
+++ src/components/activities/templates/template1.tsx
@@ -1,9 +1,7 @@
-import { ClockIcon, MapPinIcon } from "@heroicons/react/24/solid";
-import Image from "next/image";
 import Link from "next/link";
 import { AnotherActivities } from "../anotherActivities";
 import { JSX } from "react";
-import placeholderImage from "@/public/placeholder.webp";
+import { Hero } from "@/components/hero";
 
 // Základní template pro aktivity
 export const ActivityTemplate1 = ({
@@ -35,72 +33,13 @@ export const ActivityTemplate1 = ({
 }) => {
   return (
     <div className="flex flex-col items-center w-full min-h-screen mb-24">
-      {/* big screen view */}
-      <div className="hidden sm:flex relative w-[calc(100vw-15px)] h-[60vh] min-h-[500px]">
-        <Image 
-          src={`/${image}` || placeholderImage} 
-          alt={name} 
-          fill
-          className="object-cover"
-        />
-        <div className="absolute inset-0 bg-black/60" />
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
-          <div className="max-w-4xl flex flex-col gap-y-10">
-            <h1 className="text-5xl text-white font-bold">{name}</h1>
-            <div className="flex justify-center gap-x-10 text-black font-semibold uppercase text-sm">
-              {
-                time?.map((time, index) => {
-                  return (
-                    <p key={index} className="flex items-center gap-x-2 bg-yellow py-1.5 px-3 rounded-xl">
-                      <ClockIcon className="w-6 h-6 text-black" />
-                      {time}
-                    </p>
-                  )
-                })
-              }
-              <p className="flex items-center gap-x-2 bg-yellow py-1.5 px-3 rounded-xl">
-                <MapPinIcon className="w-6 h-6 text-black" />
-                {place}
-              </p>
-            </div>           
-          </div>
-        </div>
-      </div>
-      {/* mobile screen view */}
-      <div className="flex sm:hidden items-center w-full">
-        <div className="w-full flex flex-col justify-center item-center gap-y-10">
-          <h1 className="text-4xl text-gray-9 font-bold">{name}</h1>
-          <div className="flex justify-center">
-            <Image src={`/${image}` || placeholderImage} alt={name} width={300} height={300} className="w-full aspect-square rounded-4xl" />
-          </div>
-          <div className="flex flex-col gap-y-8 text-blue-5 uppercase text-sm">
-            { 
-            time?.map((time, index) => {
-              return (
-                <p key={index} className="flex items-center gap-x-2 bg-yellow py-1.5 px-3 rounded-xl">
-                  <ClockIcon className="w-6 h-6 text-black" />
-                  {time}
-                </p>
-            )}
-            )}           
-            <p className="flex items-center gap-x-2 bg-yellow py-1.5 px-3 rounded-xl">
-              <MapPinIcon className="w-6 h-6 text-black" />
-              {place}
-            </p>
-          </div>
-          <div className="text-gray-9 text-base flex flex-col items-start gap-y-8">
-            {
-              description.map((paragraph, index) => {
-                return (
-                  <p key={index}>{paragraph}</p>
-                )
-              })
-            }
-          </div>          
-        </div>
-        
-      </div>
-      <div className="text-gray-9 text-base flex flex-col items-center gap-y-8 w-1/2 my-24">
+      <Hero 
+        name={name} 
+        time={time} 
+        place={place} 
+        image={image} 
+      />
+      <div className="text-gray-9 text-base flex flex-col items-center gap-y-8 w-5/6 sm:w-1/2 my-24">
         {
           description.map((paragraph, index) => {
             return (
