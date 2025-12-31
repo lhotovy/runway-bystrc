5a35778d7647e11b1d6ad48a2a78ea2a0557c1de
lhotovy <lhotovy@centrum.cz>
2025-12-31 08:24:03 +0100

    Sundays page series

--- src/data/templates/template2Data.ts
+++ src/data/templates/template2Data.ts
@@ -8,8 +8,8 @@ export const template2Data = {
             anotherServicesTitle: "Nemůžete přijít k nám?",
             anotherServicesText: "Jsme součástí Baptistů v Brně a naše akce se konají i na dalších místech.",
             anotherServicesLink: "Kam jinam v Brně zajít",
-            currentTopicTitle: "Vánoce na Runwayi",
-            currentTopicText: "Všichni slaví Vánoce – světýlka, stromečky, dárky, tradice... Může nám ale jejich význam něco říct i dnes? V prosincové minisérii se podíváme na to, jestli a jak se příběh Ježíšova narození dotýká našich životů."
+            currentTopicTitle: "A je tu leden",
+            currentTopicText: "A je tu leden. A s ním i novinky na Nedělích na Runwayi. Nově můžete přijít každý týden! První a třetí neděli v měsíci na klasickou bohoslužbu a ty zbývající na Chosen club - promítání seriálu Chosen s následnou diskuzí. Těšíme se na Vás i v lednu!"
         },
         en: {
             anotherContacts: {
@@ -19,8 +19,8 @@ export const template2Data = {
             anotherServicesTitle: "Can't come to us?",
             anotherServicesText: "We are part of Baptist in Brno and our events are also held at other places.",
             anotherServicesLink: "Where to go in Brno",
-            currentTopicTitle: "Christmas on Runway",
-            currentTopicText: "Everyone celebrates Christmas – candles, trees, gifts, traditions... But can its meaning still say something to us today? In the December miniseries, we'll look at how the story of Jesus' birth touches our lives."
+            currentTopicTitle: "January is here",
+            currentTopicText: "January is here. And with it new updates on Sundays at Runway. You can now come every week! On the first and third Sundays of the month to the classic worship service and the remaining ones to the Chosen club - watching the Chosen series with a follow-up discussion. We look forward to seeing you in January!"
         },
     }
 }
