ebafa323faef1ca44f67adca283f0be26308f0bd
lhotovy <lhotovy@centrum.cz>
2026-06-09 15:24:20 +0200

    June series

--- src/data/templates/template2Data.ts
+++ src/data/templates/template2Data.ts
@@ -8,8 +8,8 @@ export const template2Data = {
             anotherServicesTitle: "Nemůžete přijít k nám?",
             anotherServicesText: "Jsme součástí Baptistů v Brně a naše akce se konají i na dalších místech.",
             anotherServicesLink: "Kam jinam v Brně zajít",
-            currentTopicTitle: "Květen",
-            currentTopicText: "V květnu otevíráme sérii s názvem Být v Boží blízkosti. Dále pokračujeme také v Chosen clubech, které se s bohoslužbami střídají. Těšíme se na Vás vždy v neděli v 16:00 v Runway Hub."
+            currentTopicTitle: "Červen",
+            currentTopicText: "V červnu budeme pokračovat dál v sérii \"Být v Boží blízkosti\" a také v Chosen clubech, které se s bohoslužbami střídají. Těšíme se na Vás vždy v neděli v 16:00 v Runway Hub."
         },
         en: {
             anotherContacts: {
@@ -19,8 +19,8 @@ export const template2Data = {
             anotherServicesTitle: "Can't come to us?",
             anotherServicesText: "We are part of Baptist in Brno and our events are also held at other places.",
             anotherServicesLink: "Where to go in Brno",
-            currentTopicTitle: "May",
-            currentTopicText: "In May, we're starting a series called Being in God's Presence. We'll also continue with Chosen clubs, which alternate with our services. We look forward to seeing you every Sunday at 4:00 PM at Runway Hub."
+            currentTopicTitle: "June",
+            currentTopicText: "In June, we'll continue with the series \"Being in God's Presence\" and also with Chosen clubs, which alternate with our services. We look forward to seeing you every Sunday at 4:00 PM at Runway Hub."
         },
     }
 }
